8f0c7ad827821fc2ce1b7a49254d8b2f63f41a5d
Jhosep Guadalupe <jhosepgc14@gmail.com>
2021-01-02 17:44:04 -0500

    empezando en flutter - StatefulWidget

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//widgets - our files
+import 'package:contador/src/app.dart';
+
+//funcion principal
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final styleText = new TextStyle(fontSize: 30);
+
+  final conteo = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Titulo"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Número de Tabs:",
+              style: styleText,
+            ),
+            Text(
+              "$conteo",
+              style: styleText,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print("Hola Mundoooo!")},
+        child: Icon(Icons.add_circle_outline),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
